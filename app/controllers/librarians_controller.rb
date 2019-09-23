83b443319f9ff05b27cf6b4322ae0f97d4f9cf42
bpatel24 <bpatel24@ncsu.edu>
2019-09-22 21:16:42 -0400

    Adding case for not allowing signup student who is already a librarian and viceversa

--- app/controllers/librarians_controller.rb
+++ app/controllers/librarians_controller.rb
@@ -22,13 +22,20 @@ class LibrariansController < ApplicationController
 
   def create
     @librarian = Librarian.new(librarian_params)
-    if @librarian.save
-      flash[:notice] = "Librarian created successfully"
+    ####to find if user is already student
+    student = Student.find_by_email(@librarian[:email])
+    if student == nil
+            ######
+          if @librarian.save
+            flash[:notice] = "Librarian created successfully"
+            redirect_to root_path
+          else
+            render "librarians/new"
+          end
+    else 
+      flash[:notice] = "Account already created as Student"
       redirect_to root_path
-    else
-      render "librarians/new"
     end
-
   end
 
 end
