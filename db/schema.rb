127b4f61aa2373180fdaf210f92437e939b0dd6e
Devarsh <devarshshah98@gmail.com>
2019-09-27 23:45:32 -0400

    Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_024906) do
+ActiveRecord::Schema.define(version: 2019_09_28_001155) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "librarians", force: :cascade do |t|
     t.string "email"
