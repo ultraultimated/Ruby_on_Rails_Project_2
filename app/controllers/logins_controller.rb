a5a853c5c997babeebc4257c58e3eb5c000de501
neelparikh <neelp1543@gmail.com>
2019-10-02 00:45:41 -0400

    session management for libraraian

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,13 +5,14 @@ class LoginsController < ApplicationController
     params.require(:login).permit(:email, :password)
   end
 
-    public
+  public
 
   def new
     @login = Login.new
   end
 
   def create
+
     @login = Login.new(login_params)
     student = Student.find_by_email(@login[:email])
     if student&.authenticate(params[:login][:password])
@@ -20,39 +21,41 @@ class LoginsController < ApplicationController
       session[:university_id] = student.university_id
       redirect_to :controller => 'students', :action => 'index'
     elsif Librarian.find_by_email(@login[:email])
-        librarian = Librarian.find_by_email(@login[:email])
-        if librarian&.authenticate(params[:login][:password])
-          session[:librarian_id] = librarian.id
-          session[:role] = "librarian"
+      puts "***********"
+      puts "in librarian"
+      librarian = Librarian.find_by_email(@login[:email])
+      if librarian&.authenticate(params[:login][:password])
+        session[:librarian_id] = librarian.id
+        session[:role] = "librarian"
 
-          session[:library] = librarian[:library_id]
-          puts session[:library]
-          redirect_to :controller => 'librarians', :action => 'index'
-        else
-          flash[:notice] = "Invalid Credentials"
-          redirect_to root_path
-        end
-      elsif Admin.find_by_email(@login[:email])
-        admin = Admin.find_by_email(@login[:email])
-        if admin&.authenticate(params[:login][:password])
-          session[:admin_id] = admin.id
-          session[:role] = "admin"
-          redirect_to :controller => 'admins', :action => 'index'
-        else
-          flash[:notice] = "Invalid Credentials"
-          redirect_to root_path
-        end
+        session[:library] = librarian[:library_id]
+        puts session[:library]
+        redirect_to :controller => 'librarians', :action => 'index'
+      else
+        flash[:notice] = "Invalid Credentials"
+        redirect_to root_path
+      end
+    elsif Admin.find_by_email(@login[:email])
+      admin = Admin.find_by_email(@login[:email])
+      if admin&.authenticate(params[:login][:password])
+        session[:admin_id] = admin.id
+        session[:role] = "admin"
+        redirect_to :controller => 'admins', :action => 'index'
+      else
+        flash[:notice] = "Invalid Credentials"
+        redirect_to root_path
+      end
     end
   end
 
-   def destroy
+  def destroy
     # Remove the user id from the session
     if session[:student_id]
       session[:student_id] = nil
     elsif session[:librarian_id]
       session[:librarian_id] = nil
     elsif session[:admin_id]
-      session[:admin_id]=nil
+      session[:admin_id] = nil
     end
     redirect_to root_url
   end
